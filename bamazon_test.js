2852859abb0b3964ab95ad65fce9ee6d45a3ca41
Preston Edwards <pres@imdiversity.com>
2019-03-02 10:03:49 -0600

    table fixed

--- bamazon_test.js
+++ bamazon_test.js
@@ -8,35 +8,28 @@ user: "root",
 password: "Learn2code$",
 database: "bamazon_DB"
 })
-function displayTable(){
-    connection.query("SELECT * FROM products", function(err, data){
-        if (err){
-            console.log(err)
-        }
-        console.log('\n')
-         console.table(data)
-    })
-};
-
 connection.connect(function(err){
     
-    console.log("connected as id: "+connection.threadId);
-    
-    start();
+    return console.log("connected as id: "+connection.threadId);
 })
-
-var start = function (){
+connection.query("SELECT * FROM products", function(err, data){
+    if (err){
+        console.log(err)
+    }
+    console.table(data)
+} )
+function start(){
     inquirer.prompt([{
         name: "item_id",
         type: "input",
-        message: "What is the item id of the product you would like to purchase?",
+        message: "What is the item id of the product you would like to purchase",
     },{
             name: "quantity",
             type: "input",
-            message: "How many products you would like to purchase?",
+            message: "How many products you would like to purchase",
     }]).then(function(answer){  
         
-        console.log(answer)
+       return console.log(answer)
         //connection.query("SELECT * FROM products WHERE item_id = ?", [answer.item_id], function(err, data){
         //     if (err){
         //         console.log(err)
@@ -48,17 +41,7 @@ var start = function (){
         // } else {
         //     //bidAution();
         // }
-        connection.query("SELECT * FROM products WHERE item_id = ?", [answer.item_id], function(err, data){
-            if (err){
-                console.log(err)
-            }
-            console.log(data)
-            
-            
-        } )
         
     })
-    
 }
-start();
-displayTable();
+start()
